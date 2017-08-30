9a9ea8ceed49db4521d92b598fb8f28aad694758
rajarshiwd <rajarshiwd@gmail.com>
2017-08-30 00:31:15 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+
+
+const express = require ('express');
+const app = express();
+const hbs = require('hbs');
+var fs = require('fs');
+
+app.set('view engine','hbs');
+
+//registering partial views
+hbs.registerPartials(__dirname +'/views/partials');
+
+//registering Helper functions
+hbs.registerHelper('currentYear',()=>{
+  return new Date().getFullYear();
+})
+
+// middleware to use files in other folders
+app.use(express.static(__dirname +'/public'));
+
+// middleware to print the log
+app.use((req,res,next) =>{
+
+  var now = new Date().toString();
+  var log = `${now} : ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('serverLog',log+'/n',(error)=>{
+    if(error){
+      console.log(error);
+    }
+  });
+  next();
+});
+// middleware
+app.use((req,res)=>{
+  res.render('maintainence.hbs')
+})
+
+app.get('/',(request,response)=>{
+response.render('home.hbs',{
+  page:'Home Page',
+
+});
+});
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    page:'About Page',
+    date: new Date().getFullYear()
+  })
+})
+app.listen(4200);
